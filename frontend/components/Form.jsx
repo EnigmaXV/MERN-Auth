8440a22a59dfbbd206beada9957acca0ee1aaa37
Omar Awad <omarawad@Mac.fritz.box>
2025-04-08 00:29:52 +0200

    add: enhance styling of Form component with background color and backdrop filter

--- frontend/components/Form.jsx
+++ frontend/components/Form.jsx
@@ -119,6 +119,9 @@ const StyledWrapper = styled.div`
     z-index: 1;
     margin: 0 auto;
     box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+    background-color: rgba(26, 26, 26, 0.6);
+    backdrop-filter: blur(8px);
+    -webkit-backdrop-filter: blur(8px);
   }
 
   .title {
