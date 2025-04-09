af19038fff2e8d9a242f3da5d7bc07fea6b2e1e7
Omar Awad <omarawad@Mac.fritz.box>
2025-04-09 02:39:03 +0200

    add: implement HomePage, LoginPage, and SignupPage components with toast notifications and routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import styled from "styled-components";
-import Pattern from "../components/Pattern";
+import Pattern from "./components/Pattern";
 import { Routes, Route } from "react-router-dom";
-import SignupPage from "../pages/SignupPage";
+import SignupPage from "./pages/SignupPage";
 import { Toaster } from "react-hot-toast";
-import LoginPage from "../pages/LoginPage";
+import LoginPage from "./pages/LoginPage";
+import EmailVerificationPage from "./pages/EmailVerificationPage";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
   return (
@@ -13,6 +15,8 @@ const App = () => {
       <Routes>
         <Route index element={<SignupPage />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/verify-email" element={<EmailVerificationPage />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
       <Toaster />
     </PageWrapper>
---
+++ frontend/src/configs/toastConfigs.js
@@ -0,0 +1,22 @@
+export const toastStyle = {
+  style: {
+    minWidth: "250px",
+    backgroundColor: "#333",
+    color: "#fff",
+    borderRadius: "5px",
+    padding: "10px",
+  },
+  success: {
+    duration: 5000,
+    icon: "✅",
+  },
+
+  error: {
+    duration: 5000,
+    icon: "❌",
+  },
+  loading: {
+    duration: Infinity,
+    icon: "⏳",
+  },
+};
